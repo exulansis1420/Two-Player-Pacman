4aa30ab7c0ac60708b112e2020f763334848fc87
Tanishq <58620786+exulansis1420@users.noreply.github.com>
2021-05-17 02:20:31 +0530

    smoother pacman controls

--- pacman.cpp
+++ pacman.cpp
@@ -3,58 +3,64 @@
 PacMan::PacMan() : Entity(285,475,30,30)
 {
     eatenDots = 0;
+    dir = 0;
 }
 void PacMan::animate(SDL_Rect &textureRect, int direction, int &animstartframe) {
     //0 = DEFAULT //1 = UP, 2 = RIGHT, 3 = LEFT, 4 = DOWN
-    if(direction ==0) {
-        //no direction change
-    }
-    else if(direction ==1) {
-        animstartframe = 0;
-    }
-    else if(direction==2) {
-        animstartframe = 135;
-    }
-    else if(direction==3){
-        animstartframe = 90;
-    }
-    else{
-        animstartframe = 45;
-    }
-    int totalFrames = 3;
-    int delayPerFrame = 100;
+   
+        if(direction ==0) {
+            //no direction change
+        }
+        else if(direction ==1) {
+            animstartframe = 0;
+            
+        }
+        else if(direction==2) {
+            
+            animstartframe = 135;
+        }
+        else if(direction==3){
+            
+            animstartframe = 90;
+        }
+        else{
+
+            animstartframe = 45;
+        }
+        int totalFrames = 3;
+        int delayPerFrame = 100;
+        
+        int frame = (SDL_GetTicks() / delayPerFrame) % totalFrames;
+        textureRect.y = 0;
+        textureRect.x = animstartframe + frame * textureRect.w;
     
-    int frame = (SDL_GetTicks() / delayPerFrame) % totalFrames;
-    textureRect.y = 0;
-    textureRect.x = animstartframe + frame * textureRect.w;
-    
+    if(dir==1) {Entity::move(0,-1);}
+    else if(dir==2){Entity::move(1,0);}
+    else if(dir==3){Entity::move(-1,0);}
+    else if(dir==4){Entity::move(0,1);}
+        
+        //screenPosY.;
 }
 
 void PacMan::move(SDL_Event input, SDL_Rect &textureRect, int &animstartframe)
 {
-    int dir=0;
-    if(input.type == SDL_KEYDOWN)
-    {
-        if(input.key.keysym.sym == SDLK_UP) {
-            Entity::move(0,-1);
+    
+    const Uint8 *keys = SDL_GetKeyboardState(NULL);
+
+        if(keys[SDL_SCANCODE_UP]) {
             dir=1;
         }
-        else if(input.key.keysym.sym == SDLK_DOWN) {
-            Entity::move(0,1);
+        else if(keys[SDL_SCANCODE_DOWN]) {
             dir=4;
         }
-        else if(input.key.keysym.sym == SDLK_LEFT) {
-            Entity::move(-1,0);
+        else if(keys[SDL_SCANCODE_LEFT]) {
             dir=3;
         }
-        else if(input.key.keysym.sym == SDLK_RIGHT) {
-            Entity::move(1,0);
+        else if(keys[SDL_SCANCODE_RIGHT]) {
             dir=2;
         }
-    }
-    
+
     animate(textureRect,dir,animstartframe);
-   
 }
 
 
--- pacman.hpp
+++ pacman.hpp
@@ -8,13 +8,16 @@ class PacMan
 private:
     int eatenDots;
     bool dead;
+    int dir;
+    
 
 public:
     PacMan();
     void move(SDL_Event input, SDL_Rect &textureRect, int &animstartframe);
     void stop();
     void eatDot();
-    void animate(SDL_Rect &textureRect,int direction, int &animstartframe);
+    void spawn(SDL_Rect &textureRect);
+    void animate(SDL_Rect &textureRect, int direction, int &animstartframe);
     void eatPowerDot();
     int getDotsEaten();
     void setDead(bool d);
